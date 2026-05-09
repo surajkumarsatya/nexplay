cebe24c711c09a697e5eeadf67a15ea87240dacb
Suraj <surajmail.799@gmail.com>
2026-05-09 09:55:12 +0530

    add banner and carousel section

---
+++ client/src/components/section/BannerSection.jsx
@@ -0,0 +1,75 @@
+import { getBannerData, getWatchUrl, media } from '@/lib/api'
+import React, { Suspense } from 'react'
+// import { Skeleton } from '../atom/Skeleton';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '../ui/carousel';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Skeleton } from '../atom/Skeleton';
+
+async function BannerSection({ fetcher }) {
+  return (
+    // <Suspense fallback={<BannerSectionFallback />}>
+      <BannerSectionContent fetcher={fetcher} />
+    // </Suspense>
+  )
+}
+
+
+async function BannerSectionContent({ fetcher }) {
+  const data = await fetcher();
+  // console.log(data);
+  if (!data || data.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center w-full h-[500px] py-12">
+        <InboxIcon
+          className="w-32 h-32 text-slate-400 mb-10"
+          strokeWidth={1.2}
+        />
+        <p className="text-lg text-gray-500">No items found.</p>
+      </div>
+    );
+  }
+  return (
+    <Carousel
+      opts={{
+        align: "center",
+        loop: true,
+      }}
+      className="w-full  md:px-0 px-4"
+    >
+      <CarouselContent className="">
+        {data?.map((vid) => (
+          <CarouselItem key={vid.id} className="w-full max-w-[900px] h-[500px]">
+            <Link href={getWatchUrl(vid.id, vid.media_type, vid?.backdrop_path)}>
+              <Image
+                src={media(vid?.backdrop_path)}
+                alt=""
+                width={700}
+                height={500}
+                className="w-full h-full bg-slate-600 rounded-lg object-cover"
+                quality={30}
+              />
+            </Link>
+
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <div className="absolute !bottom-2 right-[12%] hidden md:flex" style={{bottom: "45px"}}>
+        <CarouselPrevious className="w-[60px] h-[60px]" style={{marginLeft: "-30px"}}/>
+        <CarouselNext className="w-[60px] h-[60px] !ml-3" />
+      </div>
+    </Carousel>
+  )
+}
+function BannerSectionFallback() {
+  return (
+    <div className="flex items-center justify-center gap-5">
+      <Skeleton className="h-[500px] w-[700px] rounded-lg" />
+      <Skeleton className="h-[500px] w-[700px] rounded-lg" />
+      <Skeleton className="h-[500px] w-[700px] rounded-lg" />
+    </div>
+  )
+
+}
+
+export default BannerSection;
