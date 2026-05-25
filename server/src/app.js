b371b5b8464c1301543166b97591af1d927bd693
Suraj <surajmail.799@gmail.com>
2026-05-25 11:37:28 +0530

    fix frontend url

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ import paymentRoute from "./routes/payment.route.js"
 app.use(express.json())
 app.use(cookieParser())
 const corsConfig = {
-    origin: "https://nexxplay.vercel.app/",
+    origin: "https://nexxplay.vercel.app",
     credentials: true,
 };
 app.use(cors(corsConfig));
