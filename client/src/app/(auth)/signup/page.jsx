efb79537cf5eab51d7c9523370e387855836ec53
Suraj <surajmail.799@gmail.com>
2026-07-28 10:50:10 +0530

    add import useSelect

--- client/src/app/(auth)/signup/page.jsx
+++ client/src/app/(auth)/signup/page.jsx
@@ -20,6 +20,7 @@ import { useRouter } from "next/navigation";
 import { LucideLoader2 } from "lucide-react";
 import { useDispatch } from "react-redux";
 import { userLoggedInDetails } from "@/redux/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SignupPage() {
 
